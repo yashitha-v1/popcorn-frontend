caf5c1f45a8d36ed25b347c4b1e5079b8c161418
yashitha-v1 <yashithav1@gmail.com>
2026-01-16 17:41:33 +0530

    Update authStorage.js

--- authStorage.js
+++ authStorage.js
@@ -1,19 +1,1 @@
-export function getUsers() {
-    return JSON.parse(localStorage.getItem("users")) || [];
-}
 
-export function saveUsers(users) {
-    localStorage.setItem("users", JSON.stringify(users));
-}
-
-export function setCurrentUser(user) {
-    localStorage.setItem("currentUser", JSON.stringify(user));
-}
-
-export function getCurrentUser() {
-    return JSON.parse(localStorage.getItem("currentUser"));
-}
-
-export function logoutUser() {
-    localStorage.removeItem("currentUser");
-}
